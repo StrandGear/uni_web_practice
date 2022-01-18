aa652c46aa1634a05731bf741d13968084c317e8
StrandGear <strandgear@mail.ru>
2022-01-18 21:49:37 +0300

    Massive upd

--- script.js
+++ script.js
@@ -1,44 +1,17 @@
-//data = '[{"name" : "Ashwin", "age" : "20"},{"name" : "Abhinandan", "age" : "20"}]';
-//var mydata = JSON.parse(data);
-//alert(mydata[0].name);
-
-//TEST section
-var questionContainers = document.getElementsByClassName("questionContainer") ;
-var radioNum = -1;
-
-for (let i = 1; i<questionContainers.length; i++)
-{
-    questionContainers[i].style.display = "none";
-}
-/*
-function radioClicked()
-{
-    if(i < questionContainers.length)
-    { 
-        i++;
-        questionContainers[i].style.display = "block";
-    }
-}
-
-for (let i = 1; i < questionContainers.length; i++)
-{
-    var radios = questionContainers[i].querySelectorAll('input[type=radio]');
-    for (let j = 0; j <  radios.length; j++)
-    {
-        if (radios[j].checked = true)
-        {
-        //questionContainers[i+1].style.display = "block";
-        //console.log("checked")
-        
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Закройте выпадающее меню, если пользователь щелкает за его пределами
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
         }
+      }
     }
-};
-*/
-function radioClicked(num)
-{
-    if(num != radioNum)
-    {
-        radioNum = num;
-        questionContainers[num+1].style.display = "block";
-    }
-}
+  }
